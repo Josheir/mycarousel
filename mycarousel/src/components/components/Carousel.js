b9bba14f6451d86f680900b4de9903ea7772d499
PortfolioJoshua <joshuaeirm@gmail.com>
2023-01-29 07:51:14 -0500

    Removed comments.

--- mycarousel/src/components/components/Carousel.js
+++ mycarousel/src/components/components/Carousel.js
@@ -8,7 +8,7 @@ import './Carousel.css';
 import React from 'react'
 import {useState} from 'react';
 
-//let i = 0;
+
 let backgroundImage = new Array();
 backgroundImage[0] = './image1.png';
 backgroundImage[1] = './image2.png';
@@ -16,10 +16,7 @@ backgroundImage[2] = './image3.png';
 backgroundImage[3] = './image4.png';
 backgroundImage[4] = './image5.png';
 backgroundImage[5] = './image6.png';
-//backgroundImage[6] = './image1.png';
-//backgroundImage[7] = './image2.png';
-//backgroundImage[8] = './image3.png';
-//backgroundImage[9] = './image4.png';
+
 
 var a0 = 0;
 var a1 = 0;
@@ -40,51 +37,17 @@ a3 = backgroundImage[i+3];
 a4 = backgroundImage[i+4];
 a5 = backgroundImage[i+5];
 a6 = backgroundImage[i+6];
-//a7 = backgroundImage[i];
-//a8 = backgroundImage[i +1];
-//a9 = backgroundImage[i +2];
 
 
 
 
 const MAXCOUNT = 100;
 
-        /*
-        console.log("A");
-        console.log(i);
-        //a2 = backgroundImage[i+1];
-        console.log(i+1);
-        //a3 = backgroundImage[i+2]
-        console.log(i+2);
-        //a4 = backgroundImage[i+3];
-        console.log(i+3);
-        console.log("A"); 
-
-        console.log(a1); 
-        console.log(a2);
-        console.log(a3);
-        console.log(a4);
-*/
-    
-    
-    //let counter1 = 1;
-    //let j = 4;
+        
     let k = -1;
     i = -1;
 
-    console.log("i");
-    console.log(i);
-    function ClickedArrowLeft()
-    {
-
-
-        alert("here");
-        operations();
-
-        
- 
-    }
-        //i=i+4;
+    
         console.log("B"); 
         
         
@@ -128,10 +91,7 @@ const MAXCOUNT = 100;
         a2 = A2();
         a3 = A3();
 
-        //a0 = A1();
-        //a1= A2();
-        //a2 = A3();
-        //a3 = A4();
+        
 
 
         
@@ -156,8 +116,7 @@ const MAXCOUNT = 100;
         }
         getValueOfEndNode()
         {
-            //let lastNode = vectorOfElements[this.lengthOfArray]; 
-            //return(lastNode);
+            
 
         }
         getValueofStartNode()
@@ -169,35 +128,24 @@ const MAXCOUNT = 100;
         //pushes for initilaization from left to right
         pushVector(element1){
             vectorOfElements.push(element1);
-            //return(vectorOfElements);
-            //console.log(element1);
+            
         }
         
         
         moveElementsOfArrayRight()
         {
-            let storedValue  = vectorOfElements[0];
-            //Fills all but 4th imas index 3
-            for (let i = 0; i <= (5) ; i++ )
+            let storedValue  = vectorOfElements[5];
+           
+            for (let i = 5; i >= (0) ; i-- )
             {      //all values but start move right to left replacing earlier node with farther along node
                 
-                ////////////////////////////////////////////////////////////////
-                vectorOfElements[i] = vectorOfElements[i+1];
+               
+                vectorOfElements[i+1] = vectorOfElements[i];
                
             
             }
 
-            this.storageOfStartNode = '2';
-            //vectorOfElements[5] = this.storageOfStartNode;
-            vectorOfElements[5] = storedValue;
-            console.log("here");
-           
-
-
-            
-            //vectorOfElements[6] = "/image9.png";//this.storageOfStartNode;
-            console.log(vectorOfElements);
-
+            vectorOfElements[0] = storedValue;
             
 
         }
@@ -211,14 +159,7 @@ const MAXCOUNT = 100;
             
            alert(this.storageOfStartNode);
 
-
-
-            
-            
-            console.log("here1");
-            //image1
-            console.log(this.storageOfStartNode);
-
+           
             
             
             let storedValue  = vectorOfElements[0];
@@ -226,21 +167,17 @@ const MAXCOUNT = 100;
             for (let i = 0; i <= (5) ; i++ )
             {      //all values but start move right to left replacing earlier node with farther along node
                 
-                ////////////////////////////////////////////////////////////////
+               
                 vectorOfElements[i] = vectorOfElements[i+1];
                
             
             }
 
             this.storageOfStartNode = '2';
-            //vectorOfElements[5] = this.storageOfStartNode;
+           
             vectorOfElements[5] = storedValue;
             console.log("here");
            
-
-
-            
-            //vectorOfElements[6] = "/image9.png";//this.storageOfStartNode;
             console.log(vectorOfElements);
 
             
@@ -261,7 +198,7 @@ const MAXCOUNT = 100;
         displayFirstFourCharacters()
         {
             console.log(a0 + "1 " + a1 + " " + a2 + " " + a3);
-            alert("stored");
+           
             a0 = storageArray[0];
             a1 = storageArray[1];
             a2 = storageArray[2];
@@ -274,73 +211,53 @@ const MAXCOUNT = 100;
 
 
 
-    //////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////
     
-    console.log("C");
-
+    
+    
     arrayClass.pushVector(a0);
-    console.log(vectorOfElements);
+    
     arrayClass.pushVector(a1);
-    console.log(arrayClass);
+    
     arrayClass.pushVector(a2);
-    console.log(arrayClass);
+    
     arrayClass.pushVector(a3);
-    console.log(arrayClass);
+   
     arrayClass.pushVector(a4);
-    console.log(arrayClass);
+    
     arrayClass.pushVector(a5);
-    console.log("here10");
-    console.log(arrayClass);
-    ///////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////
+   
+    
 
 
-function operations(){
+function operations1(){
 
         
         console.log(arrayClass);
         arrayClass.moveElementsOfArrayLeft();
         arrayClass.readFirstFourValues();
-        //console.log("Checking begins here");
+        
         arrayClass.displayFirstFourCharacters();
 
-        //a0 = "./image3";
+        
         
 }
 
-       
-    function checkForMaxCount(l){
-       
-        if (i >= (MAXCOUNT-1))
-        {
-            alert("here");
-            //alert(i);
-            //i = 0;
-            return(0);
-        }
-        //else if(j == 4)
-        //{j = 0;
-        //return(0);
-        //}
-         else{
-            //if i > 4 i = 0
-            l=l+1;
-            //i=l;
-            //alert(i);
-        return(l);
-        }
-    }
-    /////
+function operations2(){
 
-    function testfunc(){
-        testfunct2();
-            }
+       
+    console.log(arrayClass);
+    arrayClass.moveElementsOfArrayRight();
+    arrayClass.readFirstFourValues();
     
-        function testfunct2(){
+    arrayClass.displayFirstFourCharacters();
+
     
-            a0 = "./image3";
-        }
+    
+}
+       
+    
+
+    
 
         
 const Carousel = () => {
@@ -356,8 +273,8 @@ return (
 
   
 
-<img className = "arrows arrow" src = "arrow-left.jpg" alt = "" onClick={() => {operations(); SetCounter(counter+1);}} />
-kkkk
+<img className = "arrows arrow" src = "arrow-left.jpg" alt = "" onClick={() => {operations1(); SetCounter(counter+1);}} />
+
 
 
 
@@ -365,7 +282,7 @@ kkkk
 <img className = "images2 images" alt = ""src = {a1} />
 <img className = "images2 images" alt = "" src = {a2} />
 <img className = "images2" alt = "" src = {a3} /> 
-<img className = "arrows" src = "arrow-right.png" alt = "" />
+<img className = "arrows" src = "arrow-right.png" alt = "" onClick={() => {operations2(); SetCounter(counter+1);}}/>
 
 </div>
 
